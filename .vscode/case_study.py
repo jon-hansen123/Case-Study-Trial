41a60a9f3aa6911655d1a2bf4a7d9b41dbab3ea1
jon-hansen123 <jthans23@gmail.com>
2020-11-05 16:00:46 -0500

    Final upload and extraction of data

--- .vscode/case_study.py
+++ .vscode/case_study.py
@@ -1,4 +1,19 @@
-msg = "Hello World"
-print(msg)
+import pandas as pd
+import json as json
+import tabula
 
+# Opening all files for extraction
+j = open('data\country_codes.json')
+c = open('data\World - Real GDP Levels, Annual, Billion 2015 US Dollars.csv')
+p = 'data\Population_and_migration.pdf'        # Assigning PDF filepath to variable
+
+# Creating data frames for extraction
+gdp_levels = []
+population = []
+codes = json.load(j)
+gdp_levels = pd.read_csv(c)
+population = tabula.convert_into(p, "Population.csv", output_format= "csv", pages= 3)
+
+
+# Clean file outputs
 
