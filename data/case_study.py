2af6ce562a8b5d6a02625fa09298cd3544c38245
jon-hansen123 <jthans23@gmail.com>
2021-02-04 16:26:31 -0500

    Recreated script for more compatible extraction

--- data/case_study.py
+++ data/case_study.py
@@ -3,24 +3,43 @@ import numpy as np
 import json as json
 import tabula
 import csv
+import xlsxwriter
 
-# Opening all files for extraction
-j = open('data\country_codes.json')
-c = open('data\World - Real GDP Levels, Annual, Billion 2015 US Dollars.csv')
-p = 'data\Population_and_migration.pdf'        # Assigning PDF filepath to variable
 
-# Creating data frames for extraction
-gdp_levels = []
-population = []
-codes = {}
-codes = json.load(j)
-gdp_levels = pd.read_csv(c).fillna(0)
-tabula.convert_into(p, "Population.csv", output_format= "csv", pages= 3)
-population = pd.read_csv("Population.csv")
+# Assigning countries and codes to universal variables
+with open('data/country_codes.json') as f:
+    j = json.load(f)
+for i in j:
+    country = i["Name"]
+    country_code = i["Code"]
 
-# Clean converted file output
-def clean_output():
-    for column in population.loc[:, population.columns != 'Unnamed: 0']:
-        population[column] = population[column].str.replace(" ", "")
-        population[column] = population[column].replace("..", "0")
-    return population
+
+# c = open('data\World - Real GDP Levels, Annual, Billion 2015 US Dollars.csv')
+# p = 'data\Population_and_migration.pdf'        # Assigning PDF filepath to variable
+
+
+
+# Creating final output Excel
+output_file = xlsxwriter.Workbook('CaseStudyOutput.xlsx')
+cpi1 = output_file.add_worksheet("CPI")
+gdp2 = output_file.add_worksheet("GDP")
+population3 = output_file.add_worksheet("Population")
+
+# https://www.geeksforgeeks.org/python-create-and-write-on-excel-file-using-xlsxwriter-module/
+
+# Updating CPI tab
+cpi1.write('A1', 'Country')
+cpi1.write('B1', 'Country Code')
+cpi1.write('C1', 'CPI')
+
+# Updating GDP tab
+gdp2.write('A1', 'Country')
+gdp2.write('B1', 'Country Code')
+gdp2.write('C1', 'GDP')
+
+# Updating Population tab
+population3.write('A1', 'Country')
+population3.write('B1', 'Country Code')
+population3.write('C1', 'Population')
+
+output_file.close()
